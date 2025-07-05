e648ff14bf3e06863195d7f125264ca333673a96
RahilJamadar <rahiljamadar87@gmail.com>
2025-07-05 16:21:15 +0530

    new code

--- lab 2/express_server.js
+++ lab 2/express_server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
+})
+
+app.post("/sendingName",(req,res)=>{
+    const {name,id} = req.body;
+    console.log(`Name: ${name}, ID: ${id}`);
+    res.send(`Received name: ${name} and ID: ${id}`);
 })
